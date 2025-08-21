575f0c88f4ccdf034b95942c9bb0ef25492add23
Carlos Pedroso <carlospedroso016@gmail.com>
2025-08-21 14:13:33 -0300

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+export default (async function config() {
+  const { default: love } = await import('eslint-config-love')
+
+  return [
+    {
+      ...love,
+      files: ['**/*.js', '**/*.ts'],
+    },
+    {
+		ignores: ["**/temp.js", "config/*", "**/node_modules", "**/dist"],
+	}
+  ]
+})()
